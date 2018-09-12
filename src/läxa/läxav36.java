3e8c3dd3d987f6ecfd4de1b1e3535a6bf5ba6f9f
IgorBogdanof <42673893+IgorBogdanof@users.noreply.github.com>
2018-09-12 22:10:15 +0200

    comit 2

--- src/läxa/läxav36.java
+++ src/läxa/läxav36.java
@@ -7,5 +7,11 @@ public class läxav36 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner (System.in);
+	
+		System.out.println("vad heter du?");
+		String namn = input.nextLine();
+		
+		System.out.println("Hur gammal är du?");
+		String ålder = input.nextLine();
 	}
 }
